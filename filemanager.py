137d981e50b2da7d3d6134e7c8d399d7c3a8f18f
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-02-18 20:07:08 +0000

    Testing.

---
+++ filemanager.py
@@ -0,0 +1,17 @@
+import os, shutil
+
+path = r"A:/Files"
+file_name = os.listdir(path)
+
+folder_names = ['/csv files', '/image files', '/text files', '/ms files']
+
+
+for loop in range(0,4):
+    if not os.path.exists(path + folder_names[loop]):
+        print ((path + folder_names[loop]))
+        os.makedirs(path + folder_names[loop])
+
+
+for file in file_name:
+    if '.csv' in file and not os.path.exists(path + '/csv files' + file):
+        shutil.move(path + file, path + '/csv files' + file)
