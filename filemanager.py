e9408923af65b2d9a9b82d9d05667c62b460891d
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-02-24 23:09:59 +0000

    Update filemanager.py

--- filemanager.py
+++ filemanager.py
@@ -5,7 +5,7 @@ file_name = os.listdir(path)
 
 folder_names = ['/csv files', '/image files', '/text files', '/ms files']
 
-
+##Fixed
 for loop in range(0,4):
     if not os.path.exists(path + folder_names[loop]):
         print ((path + folder_names[loop]))
